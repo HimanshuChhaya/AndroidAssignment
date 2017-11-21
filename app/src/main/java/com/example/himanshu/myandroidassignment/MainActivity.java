6a12a74391dc3cc60f5ad4dc5af4f1c548ddde6a
2016ht13336 <2016ht13336@wilp.bits-pilani.ac.in>
2017-11-21 08:20:23 +0530

    Added some files

--- app/src/main/java/com/example/himanshu/myandroidassignment/MainActivity.java
+++ app/src/main/java/com/example/himanshu/myandroidassignment/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String _welcomeMessage = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
