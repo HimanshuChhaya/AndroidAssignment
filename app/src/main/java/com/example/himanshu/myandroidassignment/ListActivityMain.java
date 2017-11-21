b37e0cd088c3cbdc593c899651f9e7353652116e
2016ht13336 <2016ht13336@wilp.bits-pilani.ac.in>
2017-11-21 10:26:56 +0530

    Code updated. Switch added.

--- app/src/main/java/com/example/himanshu/myandroidassignment/ListActivityMain.java
+++ app/src/main/java/com/example/himanshu/myandroidassignment/ListActivityMain.java
@@ -3,6 +3,7 @@ package com.example.himanshu.myandroidassignment;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -18,21 +19,27 @@ public class ListActivityMain extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_main);
 
-        setTitle("My List");
+        setTitle("Please Choose Option");
 
-        String[] items = { "First", "Second"};
-
-        //ListView list = (ListView) findViewById(R.id.@android_id/list)
+        String[] items = { "Play Audio", "Play Video", "Current Location"};
 
         Adapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
         getListView().setAdapter((ListAdapter) adapter);
-
     }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
 
-        Toast.makeText(getBaseContext(),"clicked", Toast.LENGTH_LONG);
+        String itemSelected = getListView().getItemAtPosition(position).toString();
+        switch (itemSelected)
+        {
+            case "Play Audio":
+                Log.d("MyApp", "Play Audio clicked");
+                break;
+            case "Play Video":
+                Log.d("MyApp", "Play Audio clicked");
+                break;
+        }
     }
 }
